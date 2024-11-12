0a75bb76894b79a1fc1f9a0587ebfef4ab73fd95
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2024-11-12 17:47:00 +0530

    feat: make target channel buffer configurable

--- internal/relay/config.go
+++ internal/relay/config.go
@@ -57,6 +57,7 @@ type ProducerCfg struct {
 	FlushFrequency    time.Duration `koanf:"flush_frequency"`
 	MaxMessageBytes   int           `koanf:"max_message_bytes"`
 	BatchSize         int           `koanf:"batch_size"`
+	BufferSize        int           `koanf:"buffer_size"`
 	FlushBatchSize    int           `koanf:"flush_batch_size"`
 	Compression       string        `koanf:"compression"` // gzip|snappy|lz4|zstd|none
 }
